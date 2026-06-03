73f99c24e2902624091c7dc060df8e842803603d
Jose Luis Sanchez <jose.sr@tecjerez.edu.mx>
2026-06-02 19:47:17 -0600

    Se agregaron el diseño de los dialogs

--- WMhospital/src/main/java/vista/Inicio.java
+++ WMhospital/src/main/java/vista/Inicio.java
@@ -507,6 +507,12 @@ public class Inicio extends javax.swing.JFrame {
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
         // TODO add your handling code here:
+        
+        DialogAnadirPaciente dialog = new DialogAnadirPaciente(this, true);
+        dialog.setLocationRelativeTo(this);
+        dialog.setVisible(true);
+        
+        
     }//GEN-LAST:event_jButton10ActionPerformed
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
